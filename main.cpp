76c4f05315ea0af93449633c5ed0c65fef81defa
LudoBernard <ludo.bernard.pro@gmail.com>
2020-09-16 15:41:13 +0200

    Improved the code and fixed some bugs.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,54 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
-using namespace std;
+bool playAgain = false;
 
-int main(){
-	
+bool Jeu(){
 	//Initialisation des variables
 	bool trouvé = false;
 	srand((int)time(0));
 	int chiffre = ((rand() % 100) + 1);
 	int tentatives = 0;
-
+	
 	//On continue à jouer tant que le chiffre n'est pas trouvé
 	while (trouvé == false) {
-		cout << "Devinez le chiffre entre 1 et 100!\n";
+		std::cout << "Devinez le chiffre entre 1 et 100!\n";
 		int essai = 0;
-		cin >> essai;
-		if(essai > chiffre){
-			cout << "Trop grand!\n";
+		std::string temp;
+		std::getline(std::cin, temp);
+		essai = std::atol(temp.c_str());
+		if (essai > chiffre) {
+			std::cout << "Trop grand!\n";
 			tentatives += 1;
-		} else if(essai < chiffre){
-			cout << "Trop petit!\n";
+		}
+		else if (essai < chiffre) {
+			std::cout << "Trop petit!\n";
 			tentatives += 1;
-		} else {
+		}
+		else if (essai == chiffre){
 			tentatives += 1;
-			cout << "Bravo vous gagnez avec " << tentatives << " tentatives!";
+			std::cout << "Bravo vous gagnez avec " << tentatives << " tentatives!\n";
 			trouvé = true;
+			playAgain = true;
 		}
 	}
-	
+	return true;
+}
+
+int main(){
+	Jeu();
+	while(playAgain == true){
+		std::cout << "Voulez-vous rejouer?(y/n)\n";
+		char answer = 'a';
+		std::cin >> answer;
+		if(answer == 'y'){
+			playAgain = false;
+			Jeu();
+		} else if(answer == 'n') {
+			std::cout << "Au revoir!";
+			break;
+		}
+	}
 }
