bdfc0835723f2af9f8ddd437927518694d6932ea
LudoBernard <ludo.bernard.pro@gmail.com>
2020-09-17 14:54:06 +0200

    Changed the game into a strategy matches picking game, optimizing code.

--- main.cpp
+++ main.cpp
@@ -5,43 +5,81 @@
 
 bool playAgain = false;
 
-bool Jeu() {
+void Jeu() {
 	//Initialisation des variables
-	bool trouvé = false;
 	srand((int)time(0));
-	int chiffre = ((rand() % 100) + 1);
-	int tentatives = 0;
+	bool playing = false;
+	int allumettes = 20;
+	int choixAllumettesJoueur;
+	bool tourJoueur = false;
 
-	//On continue à jouer tant que le chiffre n'est pas trouvé
-	while (trouvé == false) {
-		std::cout << "Devinez le chiffre entre 1 et 100!\n";
-		int essai = 0;
-		std::string temp;
-		std::getline(std::cin, temp);
-		essai = std::atol(temp.c_str());
-		if (essai > chiffre) {
-			std::cout << "Trop grand!\n";
-			tentatives += 1;
+	std::cout << "Bienvenue au jeu des allumettes! Le but du jeu est simple, ne pas prendre la derniere allumette.\nVoulez-vous jouer?(y/n) ";
+	char answer;
+	std::cin >> answer;
+	if (answer == 'y') {
+		int tourDebut = rand() % 2 + 1;
+		if (tourDebut == 1) {
+			tourJoueur == true;
+			std::cout << "Vous commencez!\n";
+			playing = true;
 		}
-		else if (essai < chiffre) {
-			std::cout << "Trop petit!\n";
-			tentatives += 1;
+		else if (tourDebut == 2) {
+			tourJoueur = false;
+			std::cout << "L'ordinateur commence!\n";
+			playing = true;
 		}
-		else if (essai == chiffre) {
-			tentatives += 1;
-			std::cout << "Bravo vous gagnez avec " << tentatives << " tentatives!\n";
-			trouvé = true;
-			playAgain = true;
+		while (playing && allumettes > 0) {
+			if (tourJoueur == true) {
+				std::cout << "Prenez 1, 2 ou 3 allumettes.\n";
+				std::cin >> choixAllumettesJoueur;
+				switch (choixAllumettesJoueur)
+				{
+				case 1:
+					allumettes -= choixAllumettesJoueur;
+					break;
+				case 2:
+					allumettes -= choixAllumettesJoueur;
+					break;
+				case 3:
+					allumettes -= choixAllumettesJoueur;
+					break;
+				default:
+					std::cout << "Vous ne pouvez pas enlever autant d'allumettes!\n";
+					continue;
+				}
+				std::cout << "Il reste " << allumettes << " allumettes!\n";
+				tourJoueur = false;
+			}
+			else if (tourJoueur == false){
+				int choixAllumettesOrdinateur = rand() % 3 + 1;
+				std::cout << "L'ordinateur prend " << choixAllumettesOrdinateur << " allumettes.\n";
+				allumettes -= choixAllumettesOrdinateur;
+				std::cout << "Il reste " << allumettes << " allumettes!\n";
+				std::cout << "A votre tour!\n";
+				tourJoueur = true;
+			};
+			
+			if(allumettes<=0 && tourJoueur){
+				std::cout << "Vous gagnez!\n";
+				playing = false;
+				playAgain = true;
+			} else if (allumettes <= 0 && !tourJoueur) {
+				std::cout << "Vous avez perdu!\n";
+				playing = false;
+				playAgain = true;
+			}
 		}
 	}
-	return true;
+	else if (answer == 'n') {
+		std::cout << "Au revoir!";
+	}
 }
 
 int main() {
 	Jeu();
 	while (playAgain == true) {
 		std::cout << "Voulez-vous rejouer?(y/n)\n";
-		char answer = 'a';
+		char answer;
 		std::cin >> answer;
 		if (answer == 'y') {
 			playAgain = false;
@@ -52,4 +90,5 @@ int main() {
 			break;
 		}
 	}
+	return 0;
 }
